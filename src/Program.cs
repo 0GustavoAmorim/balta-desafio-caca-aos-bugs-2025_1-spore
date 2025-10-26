c77b737eeac2cf93cc7eb54163d3bbd5435cac72
Gustavo Amorim <gustavoamorilsilva@gmail.com>
2025-10-26 03:17:36 -0300

    Desafio 1 - Concluido

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,14 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(x => 
+    x.UseSqlite(conn)
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
